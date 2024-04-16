d4b01f7825384795706a180be7a266c1ae66ec17
Carsten Kremser <github@carstenkremser.de>
2024-04-16 12:32:14 +0200

    Add comment: description of bonus exercises

--- src/bonus1.ts
+++ src/bonus1.ts
@@ -90,6 +90,56 @@ If you have already completed this task early, you may work on the bonus task on
 the next page.
 Before you come back, please submit this form.
 
+--------------------------------------------------------------------------------
+
+Bonus:
+Task:
+
+
+Write a function that highlights the names of the students with "=====". For each
+character in the entry, the same number of "=" characters should be used as needed
+for the name and age of the student.
+Example:
+
+
+Anton Meier (16)
+===================
+Grades: 1,4,3,1,A,*,1,2
+
+Berta Müller (17)
+==================
+Grades: A,*,1
+
+Cäsar Schmidt (17)
+===============
+Grades: A,1,*,3,2,4,5
+
+
+Bonus:
+Task:
+
+
+Restructure the student and the transcript in such a way that grades can be assigned
+to specific subjects. Afterwards, the student should be formatted with the
+corresponding grades per subject.
+
+
+Florian Weber (14)
+ ==============
+
+Noten:
+Sport: A,1,*
+Kunst: 3,2,4,5
+Mathe: 1,2,A
+
+
+Bonus average grade:
+Calculate the average grade for each student and later for the entire list of
+students. Discuss together how you want to design the calculation exactly
+(including handling undefined, weighting if one student has more grades than
+others, etc).
+
+
  */
 type Grade = undefined | 1 | 2 | 3 | 4 | 5 | 6 | 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
 
